be5cf0213a827033143255464ff0216f51d28bbc
ejshafran <ejshafran@hilma.tech>
2024-06-04 14:15:00 +0300

    Try to make usable as Neovim plugin

---
+++ plugin/tree-sitter-bi.lua
@@ -0,0 +1,25 @@
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+local group = augroup("tree-sitter-bi", { clear = true })
+
+-- Set `bi` as the filetype for `.bi` files
+autocmd({ "BufRead", "BufNewFile" }, {
+  group = group,
+  command = "set filetype=bi",
+  pattern = "*.bi",
+})
+
+local parsers = require "nvim-treesitter.parsers"
+---@type table<string, ParserInfo>
+local parser_configs = parsers.get_parser_configs()
+
+parser_configs.bi = {
+  install_info = {
+    url = "https://github.com/ej-shafran/tree-sitter-bi",
+    branch = "main",
+    files = { "src/parser.c", "src/scanner.c" },
+  },
+  filetype = "bi",
+  maintainers = { "@ej-shafran" },
+}
